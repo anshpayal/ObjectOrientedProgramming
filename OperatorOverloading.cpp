c052aee81e48636d451d40775c32a584fb7235a9
Ansh Payal <anshpayal754@gmail.com>
2021-10-12 22:38:23 +0530

    Operator overloading code

--- OperatorOverloading.cpp
+++ OperatorOverloading.cpp
@@ -31,8 +31,8 @@ int main(){
     Complex c1,c2,c3,c4;
     c1.setData(2,3);
     c2.setData(4,6);
-    //c3=c1+c2;        // when complier reach to this line, it tries to find out the right defination of '+' operator 
-                     //Because complier knows how to add the primitve type of data but it is complex type of data, Hence it throws error.
+    //c3=c1+c2;        // when compiler reach to this line, it tries to find out the right defination of '+' operator 
+                     //Because compiler knows how to add the primitve type of data but it is complex type of data, Hence it throws error.
     
     //c3=c1.add(c2);    //It means that c1 ne add function ko call kiya, add function me c2 ko pass kiya gya or function ne sum return kiya jooki c3 me aake store hogya.
     //c3.showData();
